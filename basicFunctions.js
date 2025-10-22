92e61561594e7836fbb37cfee2e5d11c62a980ff
VitaoL <victor.lustosacp@gmail.com>
2025-10-22 20:02:36 -0300

    começando

---
+++ basicFunctions.js
@@ -0,0 +1,11 @@
+function helloWorld() {
+  return 'Hello World';
+}
+
+function sumIndex(array) {
+  var newArray = [];
+  for(var i = 0; i < array.length; i++){
+    newArray.push(array[i])
+  };
+  return newArray;
+}
